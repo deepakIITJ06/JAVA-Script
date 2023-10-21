9e26b9a41735c0f5a04cfeade8b4829f3ed41a50
Deepak Kumar <135947040+deepakIITJ06@users.noreply.github.com>
2023-10-21 21:55:56 +0000

    dataTypes and variables

---
+++ begining/dataTypes.js
@@ -0,0 +1,15 @@
+let age = 18;
+let nm  = "deepak";
+let state = true;
+let home;
+console.log(age);
+// number => 2 to power 53
+// bigint   for big integer
+// string => ""
+// boolean = true/false
+// null => standalone value
+// undefined
+// symbol => uniqueness
+console.log(typeof "deepak");
+console.log(typeof null); // null is an object
+console.log(undefined);
---
+++ begining/variable.js
@@ -0,0 +1,18 @@
+const accId = 2345
+let holder = "deepak"
+var pass = "2121"
+accounCity = "patna"
+let nominie  // it will have "undefined"
+
+// accId = 74374  // not allowed
+holder = "suraj"
+pass = 642643
+accounCity = "jodhpur"
+
+/*
+not to use var
+because some problem in block scope and functional scope
+*/
+
+console.log(accId);
+console.table([accId, holder, pass, accounCity, nominie])
