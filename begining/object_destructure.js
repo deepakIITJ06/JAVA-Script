99453604b8d3b8111e6eabc3c2194aa95245978b
Deepak Kumar <135947040+deepakIITJ06@users.noreply.github.com>
2023-10-24 10:18:13 +0000

    json and object destructure

---
+++ begining/object_destructure.js
@@ -0,0 +1,22 @@
+const course = {
+    instructor: "deepak",
+    courseName: "JS in hindi",
+    fee: "500"
+}
+
+// course.instructor
+const {instructor: teacher} = course
+
+console.log(teacher);
+
+// Json
+// {
+//     "name": "deepak",
+//     "Hometown": "Patna"
+// }
+
+[
+    {},
+    {}
+    // this is also a json
+]
