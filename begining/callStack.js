9751ab184935d677381933733c90c23087516a8b
Deepak Kumar <135947040+deepakIITJ06@users.noreply.github.com>
2023-10-25 11:58:19 +0000

    call stack understanding

---
+++ begining/callStack.js
@@ -0,0 +1,16 @@
+/*
+global executional context
+functional executional context
+eval executional context
+*/
+
+/*
+global execution context =>
+all things will allocate to this (keyword) global object
+=>
+memory phase => all the variable will get undefined
+=>
+execution phase => variables will get their values and if there is any function then there will again a formation of execution environment will happen
+=> 
+if multiple function then this will happen again and again
+*/
